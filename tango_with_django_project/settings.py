41501d640f37698c4a1ec91e257c2380c7a50425
unknown <2575425s@student.gla.ac.uk>
2022-02-09 01:53:20 +0000

    Chapter 9 completed

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -88,6 +88,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
+LOGIN_URL = 'rango:login'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -104,6 +105,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = [
+'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+'django.contrib.auth.hashers.BCryptPasswordHasher',
+'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
